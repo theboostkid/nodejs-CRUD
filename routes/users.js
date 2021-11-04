7348506253c6284dc2257b65be6b36fd8a02c36a
theboostkid <russelltimothy2@gmail.com>
2021-11-03 22:35:37 -0500

    Feat: NodeJS CRUD API features added for searching via ID and posting and getting the users.

---
+++ routes/users.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const router = express.Router();
+
+const users = [];
+
+router.get("/", (req, res) => {
+  console.log(users);
+
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+
+  const userWithId = { ...user, id: uuidv4() };
+
+  users.push(userWithId);
+  res.send(`User with the name ${user.firstname} added to the DATABASE.`);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
+export default router;
